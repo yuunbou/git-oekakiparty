78c99c9283f16b3dff0061c84fb8a20c5ccad2aa
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-12-21 09:58:21 +0000

    [Add]ユーザ/管理者のMVCの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  
+  devise_for :users, controllers:{
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  devise_for :admin, skip:[:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  
+  
   devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
