df2bcb7921c261ab413cec8d0df1a54d04a7f301
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-12-21 04:54:54 +0000

    [add]ホストの許可を追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "680f629bf36f4c048e08918a298f9479.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
