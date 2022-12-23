525c8b6400313d523f6366c2b3d16b2e34923e90
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-12-23 08:02:24 +0000

    [Create]ヘッダーの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,29 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: devise_controller?
+  #before_action :configure_permitted_parameters, if: devise_controller?
 
-  def after_sign_in_path_for(resource_or_scope)
-    if resource_or_scope.is_a?(Admin)
-      admin_path
-    else
-      root_path
-    end
-  end
+  #def after_sign_in_path_for(resource_or_scope)
+    #if resource_or_scope.is_a?(Admin)
+      #admin_path
+    #else
+      #root_path
+    #end
+  #end
 
-  def after_sign_out_path_for(resource_or_scope)
-    if resource_or_scope == :user
-      root_path
-    elsif resource_or_scope == :admin
-      new_admin_session_path
-    else
-      root_path
-    end
-  end
+  #def after_sign_out_path_for(resource_or_scope)
+    #if resource_or_scope == :user
+      #root_path
+    #elsif resource_or_scope == :admin
+      #new_admin_session_path
+    #else
+      #root_path
+    #end
+  #end
 
-  protected
+  #protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-  end
+  #def configure_permitted_parameters
+    #devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  #end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  
-  root to: 'homes#'
-  
+
+  #root to: 'homes#top'
+
   devise_for :users, controllers:{
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
-  
-  
+
+
+
   devise_for :admin,　controllers: {
     registrations: "admin/registrations",
     sessions: "admin/sessions"
   }
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
