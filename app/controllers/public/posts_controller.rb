4ae1b3271780e4f76e08c4eb17d725fbeb4481aa
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-12-26 07:58:39 +0000

    [Add]postコントローラーアクション追加

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,7 +1,50 @@
 class Public::PostsController < ApplicationController
-  before_action :correct_user, only:[:edit, :update]
+  #before_action :correct_user, only:[:edit, :update]
   
+  def new
+      @post = Post.new
+  end
   
+  def create
+    post = Post.new(post_params)
+    post.type = "public"
+    #投稿のタイプ（個人投稿）
+    if @post.save!
+        redirect_to post_path(@post.id)
+    else
+        render :new
+    end
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    post = Post.find(params[:id])
+    post.type = 'public'
+    if post.update(post_params)
+        redirect_to post_path(@post.id)
+    else
+        render :edit
+    end
+  end
+  
+  def destroy
+  end
+  
+  def index
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :caption, :image, :is_status, :type)
+  end
   
   
 end
