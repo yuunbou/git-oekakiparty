0312ab4316a50039abbc742293b3f79fdc3c0113
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-12-29 15:20:13 +0000

    [Add]favoritesコントローラーアクション追記

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,9 +1,17 @@
 class Public::FavoritesController < ApplicationController
-  
+
   def create
+    post = Post.find(params[:post_id])
+    favorite = current_user.favorites.new(post_id: post.id)
+    favorite.save
+    redirect_to post_path(post.id)
   end
 
   def destroy
-  end  
-  
+    post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find_by(post_id: post.id)
+    favorite.destroy
+    redirect_to post_path(post.id)
+  end
+
 end
