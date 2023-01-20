8300db1d546677d2277ede4bda2abe5bce28509a
yuunbou <112618391+yuunbou@users.noreply.github.com>
2023-01-20 07:02:44 +0000

    [destroy]homesゲストログインコントローラー削除

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -7,14 +7,4 @@ class Public::HomesController < ApplicationController
   def about
   end
 
-  def guest_sign_in
-    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
-      user.password = SecureRandom.urlsafe_base64
-      user.nickname = "ゲストユーザー"
-    end
-    sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
-  end
-
-
 end
