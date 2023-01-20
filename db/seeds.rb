b5fd3d2869c955fb042594b5eea320801c46cd67
yuunbou <112618391+yuunbou@users.noreply.github.com>
2023-01-20 15:40:39 +0000

    [Create]作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+#管理者用ログイン
+Admin.create!(
+    email: 'admin@example.com',
+    password: 'adminoekakiparty'
+)
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
