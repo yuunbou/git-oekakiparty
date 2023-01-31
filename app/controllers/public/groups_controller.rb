6fc8338867067415cb02e8a6205cf1628978af20
yuunbou <112618391+yuunbou@users.noreply.github.com>
2023-01-31 11:16:51 +0000

    [Add]post_indexアクション追加

--- app/controllers/public/groups_controller.rb
+++ app/controllers/public/groups_controller.rb
@@ -4,7 +4,6 @@ class Public::GroupsController < ApplicationController
 
   
   #グループの新規作成
-  #グループの部屋を作る
   def new
     @group = Group.new
   end
@@ -12,11 +11,10 @@ class Public::GroupsController < ApplicationController
   #グループ作成
   def create
     @group = Group.new(group_params)
-    #@group.usersに、current_userを追加
     #グループ作成者もメンバーに含ませるための記述
     @group.owner_id = current_user.id
-    
-     #グループにメンバーを追加するため
+    #@group.usersに、current_userを追加
+    #グループにメンバーを追加するため
     @group.users << current_user
     if @group.save!
       redirect_to groups_user_path(current_user)
@@ -25,7 +23,7 @@ class Public::GroupsController < ApplicationController
     end
   end
   
-  #グループにユーザー追加
+  #ユーザーがグループに加入するアクション
   def join 
     @group = Group.find(params[:group_id])
     @group.users << current_user
@@ -39,13 +37,14 @@ class Public::GroupsController < ApplicationController
      #検索フォーム
     if params[:search].present? && params[:word].present?
       #Userモデルファイルにsearchとwordを定義
-      @users = User.search(params[:search], params[:word]).where.not(id: @group.owner_id).page(params[:page]).per(2)
+      #where.notを使ってその条件に当てはまるもの以外を全て取得
+      @users = User.search(params[:search], params[:word]).where.not(id: @group.owner_id).page(params[:page]).per(10)
     else
-      @users = User.where.not(id: @group.owner_id).page(params[:page]).per(2)
+      @users = User.where.not(id: @group.owner_id).page(params[:page]).per(10)
     end
   end
   
-  #ユーザー追加
+  #グループ作成者がユーザーを追加するアクション
   def join_user
     @group = Group.find(params[:group_id])
     @group.users << User.find(params[:user_id])
@@ -54,7 +53,7 @@ class Public::GroupsController < ApplicationController
     redirect_to group_join_group_path(@group)
   end
   
-  #ユーザーをグループから
+  #グループ作成者がユーザーをグループから外すアクション
   def join_destroy
     @group = Group.find(params[:group_id])
     #()の中にuserの引数を入れる
@@ -62,17 +61,15 @@ class Public::GroupsController < ApplicationController
     redirect_to group_join_group_path(@group)
   end
 
-  #グループ内の詳細（投稿したものなど）
+  #グループ内の詳細
   def show
     @group = Group.find(params[:id])
-    #@group.owner_id = current_user.id
   end
 
   #グループの編集
   def edit
     @group = Group.find(params[:id])
     @group_users = GroupUser.where(user_id: @user.ids)
-    #リストからユーザーを持ってきて追加するイメージ
   end
 
   #グループ編集の更新 ユーザーの追加
@@ -85,20 +82,24 @@ class Public::GroupsController < ApplicationController
     end
   end
 
+  #グループ内投稿ページ
   def post_index
     @group = Group.find(params[:group_id])
     @post = current_user.posts.new
-    #投稿したのが自分だったら？
-    #if 
+    #共通化
+    in_group_status(@group)
+    #作った人のowner_idとログインしているユーザーのidが同じだった場合
+    #if @group.owner_id == current_user.id
       #全て表示
-      #@posts = @group.posts
+     # @posts = @group.posts
     #else
       #publishedのみ表示
-      @posts = @group.posts.published
+      #@posts = @group.posts.published
     #end
 
   end
   
+  #ユーザーが自主的にグループから抜ける為のアクション
   def destroy
     @group = Group.find(params[:id])
     #current_userは、@group.usersから消されるという記述。
@@ -106,6 +107,7 @@ class Public::GroupsController < ApplicationController
     redirect_to user_path(current_user.id)
   end
   
+  #グループ自体を消すアクション
   def all_destroy
     @group = Group.find(params[:group_id])
     if @group.destroy
@@ -121,11 +123,19 @@ class Public::GroupsController < ApplicationController
     params.require(:group).permit(:name, :content, :group_image)
   end
   
-
   def correct_user
     @group = Group.find(params[:id])
     @user = @group.users
     redirect_to(posts_path) unless @group.owner_id == current_user.id
   end
-
+  
+  def in_group_status(group)
+    if group.owner_id == current_user.id
+      #全て表示
+      @posts = @group.posts
+    else
+      #publishedのみ表示
+      @posts = group.posts.published
+    end
+  end
 end
