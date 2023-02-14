fce92c93bc4b4740e958c46e5f78ebf98808879f
yuunbou <112618391+yuunbou@users.noreply.github.com>
2023-02-14 11:31:47 +0000

    [Add]テストデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
 #管理者用ログイン
-Admin.create!(
-    email: 'admin@example.com',
-    password: 'adminoekakiparty'
-)
+Admin.find_or_create_by!(email: 'admin@example.com') do |admin|
+    admin.password = 'adminoekakiparty'
+end
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -11,3 +10,50 @@ Admin.create!(
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# ユーザーの作成
+user = User.create!(
+    email: "test@example.com",
+    password: "testtest",
+    nickname: "test太郎",
+    is_active: "true"
+)
+
+User.create!(
+    email: "sample@example.com",
+    password: "samplesample",
+    nickname: "sample花子",
+    is_active: "true"
+)
+
+# ポストにはタイトル・キャプション・画像・タグ・post_type（個人投稿）のデータを入れる
+# タグのテストデータの作成方法
+post1 = user.posts.create!(
+    title: "test投稿",
+    post_type: 0,
+    images: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/no_group_image.jpg"),
+    filename: "no_group_image.jpg"),
+    caption: "サンプルキャプション"
+)
+
+post1.tags.create!(
+    tag_name:"sample"
+)
+
+# グループの作成
+user.groups.create!(
+    name: "testグループ",
+    content: "サンプルグループ内容",
+    owner_id: 1
+)
+    
+# グループ内投稿の作成
+user.posts.create!(
+    title: "test投稿",
+    post_type: 1,
+    images: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/no_group_image.jpg"),
+    filename: "no_group_image.jpg"),
+    group_id: 1
+)
+
+
