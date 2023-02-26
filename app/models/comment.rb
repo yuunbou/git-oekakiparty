f68ba22d9f6b25b1387cc78c2ea6034148036fd5
yuunbou <112618391+yuunbou@users.noreply.github.com>
2023-02-26 16:14:18 +0000

    [Add]バリデーション追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ApplicationRecord
 
   belongs_to :user
   belongs_to :post
+  
+  validates :comment, presence: true, length: { maximum: 1000 }
+  # validates :comment, allow_blank: true, length: { minimum: 10, maximum: 2000 }
+  
 end
