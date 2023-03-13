1cd4c8845813eae72a5986e1a95ed78091d01eb8
yuunbou <112618391+yuunbou@users.noreply.github.com>
2023-03-13 16:10:03 +0000

    [Add]テストコード追加

--- spec/system/02_after_login_spec.rb
+++ spec/system/02_after_login_spec.rb
@@ -39,4 +39,29 @@ describe '[STEP2] ユーザログイン後のテスト' do
       end
     end
   end
+  
+  describe '検索画面のテスト' do
+    before do
+      visit search_index_posts_path
+    end
+
+    context '表示内容の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/posts/search_index'
+      end
+      it '自分と他人の画像のリンク先が正しい' do
+        expect(page).to have_link '', href: user_path(post.user)
+        expect(page).to have_link '', href: user_path(other_post.user)
+      end
+      # 下記エラーを修正する
+      #it '自分の投稿と他人の投稿のタイトルのリンク先がそれぞれ正しい' do
+        #expect(page).to have_link post.title, href: post_path(post)
+        #expect(page).to have_link other_post.title, href: post_path(other_post)
+      #end
+      #it '自分の投稿と他人の投稿のタグが表示される' do
+        #expect(page).to have_content tag.tag_name
+        #expect(page).to have_content other_tag.tag_name
+      #end
+    end
+  end
 end
