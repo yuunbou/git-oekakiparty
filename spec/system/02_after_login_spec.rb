91848ed054e462f5edf2e57c17856b899594fb2d
yuunbou <112618391+yuunbou@users.noreply.github.co>
2023-03-16 14:42:50 +0000

    [Add]テストコード追加

--- spec/system/02_after_login_spec.rb
+++ spec/system/02_after_login_spec.rb
@@ -49,19 +49,18 @@ describe '[STEP2] ユーザログイン後のテスト' do
       it 'URLが正しい' do
         expect(current_path).to eq '/posts/search_index'
       end
-      it '自分と他人の画像のリンク先が正しい' do
+      it '自分と他人の投稿画像のリンク先が正しい' do
+        expect(page).to have_link '', href: post_path(post)
+        expect(page).to have_link '', href: post_path(other_post)
+      end
+      it '自分と他人のプロフィール画像のリンク先が正しい' do
         expect(page).to have_link '', href: user_path(post.user)
         expect(page).to have_link '', href: user_path(other_post.user)
       end
-      # 下記エラーを修正する
-      #it '自分の投稿と他人の投稿のタイトルのリンク先がそれぞれ正しい' do
-        #expect(page).to have_link post.title, href: post_path(post)
-        #expect(page).to have_link other_post.title, href: post_path(other_post)
-      #end
-      #it '自分の投稿と他人の投稿のタグが表示される' do
-        #expect(page).to have_content tag.tag_name
-        #expect(page).to have_content other_tag.tag_name
-      #end
+      it '自分と他人のニックネームが表示される' do
+        expect(page).to have_content user.nickname
+        expect(page).to have_content other_user.nickname
+      end
     end
   end
 end
